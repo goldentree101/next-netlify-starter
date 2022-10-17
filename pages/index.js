2b4f45e822c9d96bcfec6821bf6114001be38d47
townandcountryhomes <design@townandcountryhomes.nz>
2022-10-17 22:13:46 +1300

    changes

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          This was written by <span style={{fontSize: 1.2+'em', fontWeight:600, color:'red'}}>EB</span>
         </p>
       </main>
 
